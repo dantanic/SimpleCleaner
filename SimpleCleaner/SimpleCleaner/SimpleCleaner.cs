67ad59795d935823029405b5039cdccc695fd399
Herb9 <pksxtm@gmail.com>
2017-02-07 00:00:38 +0900

    Added command overriding function in plugin.

--- SimpleCleaner/SimpleCleaner/SimpleCleaner.cs
+++ SimpleCleaner/SimpleCleaner/SimpleCleaner.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+/*
+    ooooo   ooooo                     .o8        .ooooo.   
+    `888'   `888'                    "888       888' `Y88. 
+     888     888   .ooooo.  oooo d8b  888oooo.  888    888 
+     888ooooo888  d88' `88b `888""8P  d88' `88b  `Vbood888 
+     888     888  888ooo888  888      888   888       888' 
+     888     888  888    .o  888      888   888     .88P'  
+    o888o   o888o `Y8bod8P' d888b     `Y8bod8P'   .oP'     
+
+    Directed by Herb9.
+*/
+
+using System;
+
+using MiNET;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
 
 namespace SimpleCleaner
 {
-    public class SimpleCleaner
+
+    public class SimpleCleaner : Plugin
     {
+
+        [Command(
+            Name = "cls"
+            )]
+        public void CleanUpCommand(Player sender)
+        {
+
+        }
     }
 }
