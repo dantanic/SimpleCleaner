570053eb56fbeadbfb85d466bd96cd8e25f9c368
Herb9 <pksxtm@gmail.com>
2017-02-27 22:52:25 +0900

    Released v1.3

--- SimpleCleaner/SimpleCleaner/SimpleCleaner.cs
+++ SimpleCleaner/SimpleCleaner/SimpleCleaner.cs
@@ -15,16 +15,12 @@ using MiNET;
 using MiNET.Plugins;
 using MiNET.Plugins.Attributes;
 
-using Newtonsoft.Json.Linq;
-
 using System;
-using System.Net;
-using System.Text;
 
 namespace SimpleCleaner
 {
 
-    [Plugin(PluginName = "SimpleCleaner", Description = "The easiest way to clean your chat.", PluginVersion = "1.2", Author = "Herb9")]
+    [Plugin(PluginName = "SimpleCleaner", Description = "The easiest way to clean your chat.", PluginVersion = "1.3", Author = "Herb9")]
     public class SimpleCleaner : Plugin
     {
 
@@ -32,27 +28,14 @@ namespace SimpleCleaner
 
         protected override void OnEnable()
         {
-            CheckUpdate();
+            Console.WriteLine(Prefix + " You are currently using SimpleCleaner v1.3");
         }
 
-        [Command(Name = "cls", Description = "Cleaning chat command.")]
+        [Command(Name = "cls", Description = "Cleaning chat command.", Permission = "simplecleaner.command.cls")]
         public void ClsCommand(Player sender)
         {
             for (int i = 0; i < 20; ++i)
                 sender.SendMessage("§f\n");
-        }
-
-        private void CheckUpdate()
-        {
-            byte[] url = Convert.FromBase64String("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0hlcmJQbHVnaW5zL1NpbXBsZUNsZWFuZXIvbWFzdGVyL1NpbXBsZUNsZWFuZXIvU2ltcGxlQ2xlYW5lci9wbHVnaW4uanNvbg==");
-
-            dynamic update = JObject.Parse(new WebClient().DownloadString(Encoding.UTF8.GetString(url)));
-
-            if (update.Version > 1.2)
-                Console.WriteLine(Prefix + " New version has been found, Please inquire developer or download new version.");
-
-            else
-                Console.WriteLine(Prefix + " SimpleCleaner v1.2 successfully enabled.");
         }
     }
 }
